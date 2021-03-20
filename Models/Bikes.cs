9beaf6d4dfe43500da56b90b5eb529158a915524
Marie Floissac--Duforez <marie.floissac-duforez@epita.fr>
2021-03-20 17:14:30 +0000

    with gitignore and first model

---
+++ Models/Bikes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DublinBikes_Macintosh.Models
+{
+    public class Bikes
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [Range(0, 200)]
+        public int Number { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(80, MinimumLength = 3)]
+        public string Address { get; set; }
+
+        [Required]
+        [Range(-90, 90)]
+        public double Latitude { get; set; }
+
+        [Required]
+        [Range(-90, 90)]
+        public double Longitude { get; set; }
+
+    }
+}
