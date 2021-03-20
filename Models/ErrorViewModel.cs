dc3d9d57c1cf5ba9e5a3594f179c3494c45254cd
Marie Floissac--Duforez <marie.floissac-duforez@epita.fr>
2021-03-20 16:34:49 +0000

    1srt

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DublinBikes_Macintosh.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
